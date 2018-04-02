5438c968b9a0cb9fb594fc01deb26755d1d8c962
N8-Des <nathan.desjardins@baxter-academy.org>
2018-04-02 15:37:56 -0400

    First Version of Code
    This is my first version. It's very broken.

---
+++ Assets/BaseCharMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharMove : MonoBehaviour {
+    public Rigidbody rb;
+    public bool baseA = false;
+    public bool leftA = false;
+    public bool rightA = false;
+    public bool downA = false;
+    public bool baseB = false;
+    public bool downB = false;
+    public bool upB = false;
+    public bool rightB = false;
+    public bool leftB = false;
+    public bool moveLeft = false;
+    public bool moveRight = false;
+    public bool isIdle = false;
+    public Vector3 moveSpeed;
+    public void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        
+    }
+    public void FixedUpdate()
+    {
+        float axisLeft = Input.GetAxis("LeftJoystickX");
+        if (Input.GetButtonDown("A"))
+        {
+            baseA = true;
+        }
+        if (Input.GetButtonDown("A") && axisLeft >= 0.7)
+        {
+            rightA = true;
+        }
+        if (axisLeft >= 0.9)
+        {
+            moveRight = true;
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            rb.velocity = moveSpeed;
+        }
+        if (axisLeft <= -0.9)
+        {
+            moveLeft = true;
+            transform.rotation = new Quaternion(0, 180, 0, 0);
+            rb.velocity = moveSpeed * -1;
+        }
+
+        if (axisLeft == 0 && (moveRight == true || moveLeft == true)) 
+        {
+            Debug.Log("No Movement");
+            moveLeft = false;
+            moveRight = false;
+            isIdle = true;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Shrek/Scripts/ShrekBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrekBase : BaseCharMove {
+    public Animator anim;
+    public void OnEnable()
+    {
+        anim = gameObject.GetComponent<Animator>();  
+    }
+	void Update () {
+        if (rightA == true)
+        {
+            anim.SetTrigger("RightA");
+            rightA = false;
+        }
+        if (moveRight == true || moveLeft == true)
+        {
+            anim.SetBool("isWalking", true);
+        }
+
+        if (isIdle == true)
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
