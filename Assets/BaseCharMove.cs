66a948d8eab4ff056a4d524846aafc417a05ec9a
N8-Des <nathan.desjardins@baxter-academy.org>
2018-04-04 16:57:31 -0400

    Game still broken.
    IEnumerators cannot get small enough in waitforseconds. You still walk instead of attacking. There is no explination for this. I've tried everything.

--- Assets/BaseCharMove.cs
+++ Assets/BaseCharMove.cs
@@ -20,6 +20,7 @@ public class BaseCharMove : MonoBehaviour {
     public float threshold = 0.7f;
     public bool neutralX = false;
     public bool neutralY = false;
+    public bool canMove = true;
     public string lastInput;
     public List<string> inputBufferList = new List<string> ();
     public Vector3 moveSpeed;
@@ -33,97 +34,120 @@ public class BaseCharMove : MonoBehaviour {
     }
     public void FixedUpdate()
     {
+        inputBuffer();
         attackUpdate();
         moveUpdate();
     }
     void attackUpdate ()
     {
-        inputBuffer();
         lastInput = getLastInput();
         if (Input.GetButtonDown("A"))
         {
+            canMove = false;
             if(neutralY && neutralX)
             {
                 baseA = true;
-            }else
+            }
+            else
             {
                 attackDir();
             }
+        }
+        else
+        {
+            canMove = true;
         }
     }
     void attackDir()
     {
         if (lastInput == "Right")
         {
-            rightA = true;
+            if (this.transform.rotation == new Quaternion(0, 180, 0, 0))
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+            else
+            {
+                rightA = true;
+                rb.velocity = new Vector3(0, 0, 0);
+            }
+        }
+        if (lastInput == "Left")
+        {
+            if (this.transform.rotation == new Quaternion(0, 0, 0, 0))
+            {
+                transform.rotation = new Quaternion(0, 180, 0, 0);
+            }
+            else
+            {
+                rightA = true;
+                rb.velocity = new Vector3(0, 0, 0);
+            }
         }
     }
-    void inputBuffer()
+    public void inputBuffer()
     {
-        {
-
-        }
         neutralX = false;
+        neutralY = false;
         if (Input.GetAxis("LeftJoystickX") > threshold)
         {
             inputBufferList.Add("Right");
-        } else if (Input.GetAxis("LeftJoystickX") < -threshold)
+        }
+        else if (Input.GetAxis("LeftJoystickX") < -threshold)
         {
             inputBufferList.Add("Left");
         }
-        else
+        else if (Input.GetAxis("LeftJoystickY") > threshold)
         {
-            inputBufferList.Add("Neutral");
-            neutralX = true;
-        }
-
-        neutralY = false;
-        if(Input.GetAxis("LeftJoystickY") > threshold)
-        {
-            inputBufferList.Add("Up");
+            inputBufferList.Add("Down");
         }
         else if (Input.GetAxis("LeftJoystickY") < -threshold)
         {
-            inputBufferList.Add("Neutral");
-            inputBufferList.Add("Down");
+            inputBufferList.Add("Up");
         }
         else
         {
+            inputBufferList.Add("Neutral");
             neutralY = true;
-        }
+            neutralX = true;
+        }                 
     }
     public string getLastInput()
     {
         int amount = inputBufferList.Count;
-        inputBufferList.RemoveRange(0, (amount - 1));
+        inputBufferList.RemoveRange(0, (amount - 8));
         return inputBufferList[0];
     }
     void moveUpdate()
     {
-        if (lastInput == "Right")
-        {
-            moveLeft = false;
-            moveRight = true;
-            transform.rotation = new Quaternion(0, 0, 0, 0);
-            rb.velocity = moveSpeed;
-            isWalking = true;
-            isIdle = false;
-        }
-        else if (lastInput == "Left")
-        {
-            moveRight = false;
-            moveLeft = true;
-            transform.rotation = new Quaternion(0, 180, 0, 0);
-            rb.velocity = -moveSpeed;
-            isWalking = true;
-            isIdle = false;
-        }   else if (isWalking && neutralX)
+        if (isWalking && neutralX || (!canMove))
         {
             isWalking = false;
             moveLeft = false;
             moveRight = false;
             rb.velocity = new Vector3(0, 0, 0);
             isIdle = true;
+        }
+        if (canMove)
+        {
+            if (lastInput == "Right")
+            {
+                moveLeft = false;
+                moveRight = true;
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                rb.velocity = moveSpeed;
+                isWalking = true;
+                isIdle = false;
+            }
+            else if (lastInput == "Left")
+            {
+                moveRight = false;
+                moveLeft = true;
+                transform.rotation = new Quaternion(0, 180, 0, 0);
+                rb.velocity = -moveSpeed;
+                isWalking = true;
+                isIdle = false;
+            }
         }
     }
 }
