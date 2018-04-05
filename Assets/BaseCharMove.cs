bc14a056124b8a21a5f78717c1bc38460a7dac60
N8-Des <nathan.desjardins@baxter-academy.org>
2018-04-05 17:29:29 -0400

    YES!
    Movement and attacking work much, much better now. Added some new animations for Shrek. Probably the version I will be editing tomorrow.

--- Assets/BaseCharMove.cs
+++ Assets/BaseCharMove.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCharMove : MonoBehaviour {
+public class BaseCharMove : MonoBehaviour
+{
     public Rigidbody rb;
-    public bool baseA = false;
     public bool leftA = false;
     public bool rightA = false;
     public bool downA = false;
@@ -21,16 +22,29 @@ public class BaseCharMove : MonoBehaviour {
     public bool neutralX = false;
     public bool neutralY = false;
     public bool canMove = true;
+    public bool canAttack = true;
     public string lastInput;
-    public List<string> inputBufferList = new List<string> ();
+    public List<string> inputBufferList = new List<string>();
     public Vector3 moveSpeed;
     public Animator anim;
+    public float BADelay;
+    public float SADelay;
+    public float UADelay;
     public void Start()
     {
         inputBufferList.Add("ShutUpCount");
         anim = gameObject.GetComponent<Animator>();
-        rb = gameObject.GetComponent<Rigidbody>();       
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
+    public virtual void BaseA()
+    {
+    }
+    public virtual void SideA()
+    {
+    }
+    public virtual void UpA()
+    {
     }
     public void FixedUpdate()
     {
@@ -38,70 +52,108 @@ public class BaseCharMove : MonoBehaviour {
         attackUpdate();
         moveUpdate();
     }
-    void attackUpdate ()
+    void attackUpdate()
     {
         lastInput = getLastInput();
-        if (Input.GetButtonDown("A"))
+        if ((Input.GetButton("A") || Input.GetKey(KeyCode.Z)) && canAttack)
         {
+            canAttack = false;
             canMove = false;
-            if(neutralY && neutralX)
+            if (neutralY && neutralX)
             {
-                baseA = true;
+                anim.SetTrigger("NeutA");
+                Invoke("BaseA", BADelay);
             }
             else
             {
-                attackDir();
+                attackDir1();
+            }
+        }
+    }
+    void attackDir1()
+    {
+        if (lastInput == "Right")
+        {
+            if (transform.rotation == new Quaternion(0, 180, 0, 0))
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                anim.SetTrigger("RightA");
+                Invoke("SideA", SADelay);
+            }
+            else
+            {
+                anim.SetTrigger("RightA");
+                Invoke("SideA", SADelay);
+            }
+        }else
+        {
+            attackDir2();
+        }
+    }
+
+    void attackDir2()
+    {
+        if (lastInput == "Left")
+        {
+            if (transform.rotation == new Quaternion(0, 0, 0, 0))
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                anim.SetTrigger("RightA");
+                Invoke("SideA", SADelay);
+            }
+            else
+            {
+                anim.SetTrigger("RightA");
+                Invoke("SideA", SADelay);
             }
         }
         else
         {
-            canMove = true;
+            attackDir3();
         }
     }
-    void attackDir()
+    void attackDir3()
     {
-        if (lastInput == "Right")
+        if (lastInput == "Up")
         {
-            if (this.transform.rotation == new Quaternion(0, 180, 0, 0))
-            {
-                transform.rotation = new Quaternion(0, 0, 0, 0);
-            }
-            else
-            {
-                rightA = true;
-                rb.velocity = new Vector3(0, 0, 0);
-            }
+            anim.SetTrigger("UpA");
+            Invoke("UpA", UADelay);
         }
-        if (lastInput == "Left")
+        else
         {
-            if (this.transform.rotation == new Quaternion(0, 0, 0, 0))
-            {
-                transform.rotation = new Quaternion(0, 180, 0, 0);
-            }
-            else
-            {
-                rightA = true;
-                rb.velocity = new Vector3(0, 0, 0);
-            }
+            attackDir4();
+        }
+    }
+    void attackDir4()
+    {
+        if (lastInput == "Down")
+        {
+            anim.SetTrigger("DownA");
+            Invoke("DownA", UADelay);
+        }
+        else
+        {
+            canAttack = true;
+            canMove = true;
         }
     }
     public void inputBuffer()
     {
         neutralX = false;
         neutralY = false;
-        if (Input.GetAxis("LeftJoystickX") > threshold)
+        if (Input.GetAxis("LeftJoystickX") > threshold || Input.GetKey(KeyCode.RightArrow))
         {
             inputBufferList.Add("Right");
         }
-        else if (Input.GetAxis("LeftJoystickX") < -threshold)
+        else if (Input.GetAxis("LeftJoystickX") < -threshold || Input.GetKey(KeyCode.LeftArrow))
         {
             inputBufferList.Add("Left");
         }
-        else if (Input.GetAxis("LeftJoystickY") > threshold)
+        else if (Input.GetAxis("LeftJoystickY") > threshold || Input.GetKey(KeyCode.DownArrow))
         {
             inputBufferList.Add("Down");
         }
-        else if (Input.GetAxis("LeftJoystickY") < -threshold)
+        else if (Input.GetAxis("LeftJoystickY") < -threshold || Input.GetKey(KeyCode.UpArrow))
         {
             inputBufferList.Add("Up");
         }
@@ -110,7 +162,7 @@ public class BaseCharMove : MonoBehaviour {
             inputBufferList.Add("Neutral");
             neutralY = true;
             neutralX = true;
-        }                 
+        }
     }
     public string getLastInput()
     {
--- Assets/Shrek/Scripts/ShrekBase.cs
+++ Assets/Shrek/Scripts/ShrekBase.cs
@@ -2,19 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShrekBase : BaseCharMove {
-    
-	void Update () {
-        if (baseA)
-        {
-            anim.SetTrigger("NeutA");
-            baseA = false;
-        }
-        if (rightA == true)
-        {
-            anim.SetTrigger("RightA");
-            rightA = false;
-        }
+public class ShrekBase : BaseCharMove
+{
+
+    void Update()
+    {
         if (moveRight == true || moveLeft == true)
         {
             anim.SetBool("isWalking", true);
@@ -24,5 +16,20 @@ public class ShrekBase : BaseCharMove {
         {
             anim.SetBool("isWalking", false);
         }
+    }
+    public override void BaseA()
+    {
+        canMove = true;
+        canAttack = true;
+    }
+    public override void SideA()
+    {
+        canMove = true;
+        canAttack = true;
+    }
+    public override void UpA()
+    {
+        canMove = true;
+        canAttack = true;
     }
 }
