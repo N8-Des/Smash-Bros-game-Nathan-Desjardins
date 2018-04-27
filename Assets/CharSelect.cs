ed726ade1216016e0f022e6fc40528af14449687
N8-Des <nathan.desjardins@baxter-academy.org>
2018-04-26 21:04:06 -0400

    UI Update 1
    Working on UI stuff. Not fun...

---
+++ Assets/CharSelect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelect : MonoBehaviour {
+    public bool neutralX = false;
+    public bool neutralY = false;
+    public float threshold = 1;
+    public string lastInput;
+    public List<GameObject> player1Char = new List<GameObject>();
+    public List<string> inputBufferList = new List<string>();
+    public int onNumber = 0;
+
+    public void Update()
+    {
+        inputBuffer();
+        testForMovement();
+    }
+    public void inputBuffer()
+    {
+        neutralX = false;
+        neutralY = false;
+        if (Input.GetAxis("LeftJoystickX") > threshold || Input.GetKey(KeyCode.RightArrow))
+        {
+            inputBufferList.Add("Right");
+        }
+        else if (Input.GetAxis("LeftJoystickX") < -threshold || Input.GetKey(KeyCode.LeftArrow))
+        {
+            inputBufferList.Add("Left");
+        }
+        else if (Input.GetAxis("LeftJoystickY") > threshold || Input.GetKey(KeyCode.DownArrow))
+        {
+            inputBufferList.Add("Down");
+        }
+        else if (Input.GetAxis("LeftJoystickY") < -threshold || Input.GetKey(KeyCode.UpArrow))
+        {
+            inputBufferList.Add("Up");
+        }
+        else
+        {
+            inputBufferList.Add("Neutral");
+            neutralY = true;
+            neutralX = true;
+        }
+    }
+    public void testForMovement()
+    {
+        lastInput = getLastInput();
+        if (lastInput == "Right")
+        {
+            onNumber += 1;
+              
+        }
+    }
+    public string getLastInput()
+    { 
+        int amount = inputBufferList.Count;
+        inputBufferList.RemoveRange(0, (amount - 8));
+        return inputBufferList[0];
+    }
+}
